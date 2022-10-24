317a08ddbb607f6d3c3577c81778c0a06000613a
Pablo Bernal <vinseth.2010@gmail.com>
2022-10-24 18:15:37 -0500

    tercera entrega

--- script.js
+++ script.js
@@ -5,157 +5,487 @@
 
 
 
-/* variables globales*/
-
-// let cafe1 = "Capuccino"
-// let cafe2 = "Latte"
-// let cafe3 = "Americano"
+// let nombre = prompt("Ingrese su nombre");
 
 
-// let valorCafe1 = 3500
-// let valorCafe2 = 4500
-// let valorCafe3 = 5000
+// let holu = document.getElementById("holu")
 
+// console.log(holu.innerText ) // solo funcioan con Id, no con colecciones (arrays)
 
-let totalCafe1 = 0
-let totalCafe2 = 0
-let totalCafe3= 0
+// holu.innerText = "Bienvenido " + nombre // aqui modifica el texto del html
 
-
-/* variable totalizadora*/
-let precioTotal = 0
+// console.log(holu.innerText); //imprime el elemento modificado 
 
 
 
-function Productos (nombre,precio, stock){
+
+//-------Agrega y modifica el container, por ende afecta a todos los elementos----------------
+
+
+// let containerr = document.getElementById("contenedor")
+
+// //holu.innerHTML = document.getElementById("holi")
+
+// containerr.innerHTML = "<h1>perraaaa<h1>" // modifica el nodo padre, afectando a los hijos "contenedor"
+
+// containerr.className = "ulNom" //agrega una clase al nodo padre "contenedor"
+
+
+//---------modifica un lista de elemntos-------
+
+// let card = document.getElementsByClassName("card") //getElements como si fuese un array
+
+// card[0].className = "ulNom"
+
+
+
+//----------EVENTOSSS 1----------------------------------
+
+
+
+// let buscar = document.getElementById("primerInput")
+
+// buscar.addEventListener("change", function () {respuestaTeclado(buscar)})
+
+// function respuestaTeclado(input){
+   
+//     alert("hola "+input.value)
+//     input.value = " ";
+
+
+// }
+
+
+
+// let boton = document.getElementById("primerBoton")
+
+// boton.addEventListener("click", function () {respuestaTeclado(buscar)})
+
+//---------FIN EVENTOS 1-----------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Productos (nombre,precio, stock, img, categ){
    
     this.nombre = nombre.toLowerCase();
     this.precio = parseFloat(precio);
     this.stock = parseInt(stock);
+    this.img = img;
+    this.categ = categ;
    }
 
-
-
-let productoA = new Productos("Capuccino",3500,12)
-let productoB = new Productos("Latte",4500,5)
-let productoC = new Productos("Americano",5000,0)
+let productoA = new Productos("Capuccino",3500,12,"media/cappuccino.png", "1")
+let productoB = new Productos("Latte",4500,5, "media/latte.png", "2")
+let productoC = new Productos("Americano",5000,1, "media/americano.png", "3")
 
 let listaProducto = [productoA,productoB,productoC] 
 
 let listaProductoJSON = JSON.stringify(listaProducto);
-
-
-// const listaProducto =[] //listado de productos dentro de un array
-// listaProducto.push(new Productos("Capuccino",3500,12));
-// listaProducto.push(new Productos("Latte",4500,5));
-// listaProducto.push(new Productos("Americano",5000,0));
+let listaNombrePorductos = listaProducto.map((pro) =>  pro.nombre+"\n" );
+let listaProductoConStock = listaProducto.filter((pro) =>  pro.stock > 0 );
 
 
 
 
 
-alert("Bienvenido a Guadualito Café")
 
-let opcion2 = prompt("Desea ver que productos hay disponibles? ").toLowerCase();
 
-if(opcion2 != "no"){
 
-   let buscar = prompt("Que producto buscas?")
 
-   let resultado = listaProducto.map((pro) =>  pro.nombre+"\n" );
-   let resultad1 = listaProducto.filter((pro) =>  pro.stock+"\n" <= 0 );
-   let resultad2 = listaProducto.filter((pro) =>  pro.nombre.includes(buscar)  );
+//  -----  input * pront ----
 
+let categoriaSelc = " "
+
+// let buscar = document.getElementById("primerInput")
+// buscar.addEventListener("keyup", ()=>{filtradoMenu(buscar.value)}) //capturado por medio del change
+
+
+
+function filtradoMenu(buscar){  
+    
+
+    let listaProductoCategoria = listaProducto.filter((pro) =>  pro.nombre.includes(buscar));
+    
+    containerr.innerHTML= " "
+    
+    if(listaProductoCategoria.length == 0){
+    
+        let card2 = document.createElement("div")
+        
+        card2.innerHTML = `<h2>No tenemos ese producto por el nombre</h2>`
+        
+        card2.className = "ulNom"
+        containerr.append(card2)
+    
+    }else{
+        reset(listaProductoCategoria)
+    }
+    
+     }
+
+
+
+// let boton = document.getElementById("primerBoton")
+// boton.addEventListener("click", filtrado)
+
+
+
+// function filtrado(){  
+    
+
+// let listaProductoCategoria = listaProducto.filter((pro) =>  pro.categ.includes(categoriaSelc));
+
+// containerr.innerHTML= " "
+
+// if(listaProductoCategoria.length == 0){
+
+//     let card2 = document.createElement("div")
+    
+//     card2.innerHTML = `<h2>No tenemos ese producto</h2>`
+    
+//     card2.className = "ulNom"
+//     containerr.append(card2)
+
+// }else{
+//     reset(listaProductoCategoria)
+// }
+
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let container1 = document.getElementById("contenedor1")
+
+function reset1 (){
+    
   
 
-  alert( resultado);
-  console.log( JSON.stringify(resultad1));
-  console.log( JSON.stringify(resultad2));
+   
+    let card = document.createElement("div")
+    
+    card.innerHTML = `
+    <h2>Hola!!, nuestro menú es el siguiente</h2>
+    <p>Escoja el tipo de café que desea opción:</p>
+    <p> Capuccino </p>
+    <p> Latte</p>
+    <p> Americano </p>
+    <input type="search" id="segundoInput">
+   
+    `
+    //<button id="segundoBoton">Buscar</button> si deseo hacer el menú con boton
+    
+    card.className = "ulNom" // clase asociada
+    
+    container1.append(card) //para que lo muestre
+   
+    
 
-
-}else{
-    alert("Vale, con gusto");
 }
 
+reset1(container1);
 
-    let opcion = prompt("Escoja el tipo de café que desea opción: \n 1- Capuccino \n 2- Latte\n 3- Americano \n ESC - escriba ESC para Salir").toLowerCase();
 
-while (opcion != "esc"){
 
-    if(opcion == 1){
-        let cantidadCafe1 = parseInt(prompt("Valor unitario: "+ productoA.precio + "\n"+"seleccione cantidad de " + productoA.nombre+ " que desea"));
-        totalCafe1 += calcularProducto(cantidadCafe1, productoA.precio);
+
+function filtradoMenu(buscar){  
+    
+
+    let listaProductoCategoria = listaProducto.filter((pro) =>  pro.nombre.includes(buscar));
+    
+    //me retonar un array nuevo 
+
+    containerr.innerHTML= " "
+    
+  
+    
+  
+   
+
+
+    if(listaProductoCategoria.length == 0){
+    
+        let card2 = document.createElement("div")
+        
+        card2.innerHTML = `<h2>No tenemos ese producto por el nombre</h2>`
+        
+        card2.className = "ulNom"
+        containerr.append(card2)
+    
+
+    }else{
+
+
+        reset(listaProductoCategoria)
+        
+        const findObject = listaProductoCategoria.find(elemento => elemento.nombre.includes(buscar)) //acceder a la referencia
+
+
+
+        let inputProducto = document.getElementById("tercerInput")
+        let cantidadPro  = 0 // variable para poder pasar el evento del input al boton
+        
+        let o = findObject.precio
+
+
+        inputProducto.addEventListener("change", ()=>{cantidadPro = inputProducto.value})
+        
+        let botonProducto = document.getElementById("tercerBoton")
+        botonProducto.addEventListener("click", ()=>{calcularProducto(cantidadPro, o)})
+
+
+
+        // let cantidad = prompt("Ingrese cuaaass")
+
+        // let o = findObject.precio
+
+        // calcularProducto(cantidad, o)
+
         
     }
+
+
+
     
-    
-    else if(opcion == 2){
-        let cantidadCafe2 = parseInt(prompt("Valor unitario: "+ productoB.precio + "\n"+ "seleccione cantidad de " + productoB.nombre + " que desea"));
-        totalCafe2 += calcularProducto(cantidadCafe2, productoB.precio);
-    }
-
-    else if(opcion == 3){
-        let cantidadCafe3= parseInt(prompt("Valor unitario: "+ productoC.precio +"seleccione cantidad de " + productoC.nombre + " que desea"));
-        totalCafe3+= calcularProducto(cantidadCafe3, productoC.precio);
-    }
-
-// llamado recursivo para que no se quede en un bucle
-
-    opcion = prompt("Escoja el tipo de café que desea opcionr: \n 1- Capuccino \n 2- Latte\n 3- Americano \n ESC - escriba ESC para Salir")
-
-}
+     }
 
 
-
-
-
-// let numero = [1,2,3,4];
-
-// numero.filter((num ) => alert(num*2))
-
-// //num = es elemento por elemnto
-
-// function calcularNum (n){
-//      n = n * 2
-// }
-
-
-// función que calcula el valor de un producto
 
 function calcularProducto (cantidad, precio){
-    precioTotal += (cantidad * precio) ;
-}
 
 
-if (precioTotal != 0){
-    alert("El precio total de su compra es: $" + precioTotal +"\n"+ "Gracias por su compra !!!")
-} else {
-    alert("Gracias por su visita")
-}
 
-//no he podido imprimer el objeto, no se como  por medio de clases
-    
-// while (opcion != "esc"){
+    let precioTotal = cantidad * precio;
+    if (precioTotal != 0){
 
-//     if(opcion == 1){
-//         let cantidadCafe1 = parseInt(prompt("Valor unitario: "+ listaProducto[0].precio + "\n"+"seleccione cantidad de " + listaProducto[0].nombre+ " que desea"));
-//         totalCafe1 += calcularProducto(cantidadCafe1, listaProducto[0].precio);
+        containerr.innerHTML= " "
         
-//     }
+        let card2 = document.createElement("div")
+        
+        card2.innerHTML = `<h2>El precio total es:  ${precioTotal} </h2>`
+        
+        card2.className = "ulNom"
+        containerr.append(card2)
+
+
+       
+    } else {
+
+        let card2 = document.createElement("div")
+        
+        card2.innerHTML = `<h2>Por favor ingrese una cantidad </h2>`
+        
+        card2.className = "ulNom"
+        containerr.append(card2)
+
+
+
+       
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let input1 = document.getElementById("segundoInput")
+// let opcion = " " // variable para poder pasar el evento del input al boton
+// input1.addEventListener("change", ()=>{filtradoMenu(opcion)})
+
+input1.addEventListener("input", ()=>{filtradoMenu(input1.value)})
+
+// let boton2 = document.getElementById("segundoBoton")
+// boton2.addEventListener("click", ()=>{filtradoMenu(opcion)})
+
+
+
+
+let containerr = document.getElementById("contenedor")
+
+function reset (lista){
     
+    containerr.innerHTML= " "
+
+for(const pro of lista){
+
+   
+    let card = document.createElement("div")
     
-//     else if(opcion == 2){
-//         let cantidadCafe2 = parseInt(prompt("Valor unitario: "+ listaProducto[1].precio + "\n"+ "seleccione cantidad de " + listaProducto[1].nombre + " que desea"));
-//         totalCafe2 += calcularProducto(cantidadCafe2, listaProducto[1].precio);
-//     }
+    card.innerHTML = `
+    <img class="img"src="${pro.img}"></img>
+    <h2>${pro.nombre}</h2>
+    <p>Valor unitario: ${pro.precio}</p>
+    <p>Ingrese la cantidad: </p>
+    <input type="text" id="tercerInput">
+    <button id="tercerBoton">Calcular</button>
+ 
+    `
 
-//     else if(opcion == 3){
-//         let cantidadCafe3= parseInt(prompt("Valor unitario: "+listaProducto[2].precio +"seleccione cantidad de " + listaProducto[2].nombre + " que desea"));
-//         totalCafe3+= calcularProducto(cantidadCafe3, listaProducto[2].precio);
-//     }
+    
+    // let cantidadCafe1 = parseInt(prompt("Valor unitario: "+ productoA.precio + "\n"+"seleccione cantidad de " + productoA.nombre+ " que desea"));
+    // totalCafe1 += calcularProducto(cantidadCafe1, productoA.precio);
 
-// // llamado recursivo para que no se quede en un bucle
+    
+    card.className = "ulNom" // clase asociada
+    
+    containerr.append(card) //para que lo muestre
+   }
+}
 
-//     opcion = prompt("Escoja el tipo de café que desea opcionr: \n 1- Capuccino \n 2- Latte\n 3- Americano \n ESC - escriba ESC para Salir")
 
+
+// boton para borrar productos
+
+
+let boton1 = document.getElementById("borrame")
+boton1.addEventListener("click", boorrarFiltrado)
+
+
+function boorrarFiltrado(){  
+
+    containerr.innerHTML= " " // los vacia
+
+   
+      
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Bienvenido a Guadualito Café")
+
+// let opcion2 = prompt("Desea ver que productos hay disponibles? ").toLowerCase();
+
+// if(opcion2 != "no"){
+
+//    let buscar = prompt("Que producto buscas?")
+
+//    let listaNombrePorductos = listaProducto.map((pro) =>  pro.nombre+"\n" );
+//    let listaProductoConStock = listaProducto.filter((pro) =>  pro.stock > 0 );
+//    let resultad2 = listaProducto.filter((pro) =>  pro.nombre.includes(buscar)  );
+
+
+
+// //   alert( listaNombrePorductos);
+// //   console.log( JSON.stringify(listaProductoConStock));
+// //   console.log( JSON.stringify(resultad2))
+
+
+// }else{
+//     alert("Vale, con gusto");
 // }
+
+//function filtrado3(opcion){  
+    
+    //     while (opcion != "esc"){
+    
+    //         if(opcion == 1){
+    
+    //             container1.innerHTML= " "
+    
+    //             reset(listaProducto);
+    
+    //             let input3 = document.getElementById("tercerInput")
+    //             let cantidad = " " // variable para poder pasar el evento del input al boton
+    
+    //             input1.addEventListener("change", ()=>{cantidad = input3.value})
+    
+    //             let boton3 = document.getElementById("tercerBoton")
+    //             boton3.addEventListener("click", ()=>{calcularProducto(cantidad, productoA.precio)})
+    
+               
+    
+    //             // let cantidadCafe1 = parseInt(prompt("Valor unitario: "+ productoA.precio + "\n"+"seleccione cantidad de " + productoA.nombre+ " que desea"));
+    //             // totalCafe1 += calcularProducto(cantidadCafe1, productoA.precio);
+    //             break
+                
+    //         }
+            
+            
+    //         else if(opcion == 2){
+    //             let cantidadCafe2 = parseInt(prompt("Valor unitario: "+ productoB.precio + "\n"+ "seleccione cantidad de " + productoB.nombre + " que desea"));
+    //             totalCafe2 += calcularProducto(cantidadCafe2, productoB.precio);
+    //             break
+        
+    //         }
+        
+    //         else if(opcion == 3){
+    //             let cantidadCafe3= parseInt(prompt("Valor unitario: "+ productoC.precio +"\n" +"seleccione cantidad de " + productoC.nombre + " que desea"));
+    //             totalCafe3+= calcularProducto(cantidadCafe3, productoC.precio);
+    //             break
+    //         }
+        
+        
+            
+        
+    //     }
+       
+    // }
